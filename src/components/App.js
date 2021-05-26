e41e01b9e83f8700c3d62e44f2332c692b85a8b4
Sławomir Dziurman <g1.slawomir.dziurman@gmail.com>
2021-05-26 11:10:35 +0200

    Let's go!

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,30 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 function App() {
+
+  const [response, setResponse] = useState('');
+
+  const callAPI = () => {
+    // https://question-mark-project-anime.herokuapp.com
+
+    fetch('https://question-mark-project-anime.herokuapp.com', {
+      method: 'GET'
+    })
+      .then(res => res.text())
+      .then(res => {
+        setResponse(res);
+      })
+  }
+
+  useEffect(() => {
+    callAPI();
+  }, []);
+
   return (
-    <h1>Do dzieła!</h1>
+    <>
+      <h1>Do dzieła!</h1>
+      <h2>{response}</h2>
+    </>
   );
 }
 
